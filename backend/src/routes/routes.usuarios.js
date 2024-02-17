ccb518f3ca04e015d306a3a67cfb96b15bc9e6f8
josevargas31 <dajozavargas@gmail.com>
2024-02-16 21:11:55 -0500

    changes controlador y ruta usuarios

--- backend/src/routes/routes.usuarios.js
+++ backend/src/routes/routes.usuarios.js
@@ -1,13 +1,13 @@
 import { Router } from "express"
-import  {actualizarUsuario, listarUsuarios, eliminarUsuario, registrarUsuario} from '../controller/controller.usuarios.js'
+import  {actualizarUsuario, listarUsuarios, buscarUsuario, desactivarUsuario, registrarUsuario} from '../controller/controller.usuarios.js'
 
 
 const routesUsuarios = Router();
 
 routesUsuarios.get('/listarUsuario', listarUsuarios);
-routesUsuarios.get('/listarUsuario', listarUsuarios);
+routesUsuarios.get('/buscarUsuario/:id_ususario', buscarUsuario);
 routesUsuarios.post('/registrarUsuario', registrarUsuario);
-routesUsuarios.delete('/eliminarUsuario/:id_ususario', eliminarUsuario);
+routesUsuarios.post('/desactivarUsuario/:id_ususario', desactivarUsuario);
 routesUsuarios.put('/actualizarUsuario/:id_ususario', actualizarUsuario);
 
 export default routesUsuarios;
